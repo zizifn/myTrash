df175505e3ea3d74d2ccb04d2b1ae80460faf997
zizifn <515868058@qq.com>
2023-05-01 04:38:54 +0800

    add mock API

---
+++ src/app/api/mock/kv/route.ts
@@ -0,0 +1,11 @@
+import { CFWokersEnv } from "@/app/env";
+
+export async function GET(request: Request) {
+  console.log(process.env as any as CFWokersEnv);
+  const task = await (process.env as any as CFWokersEnv).MY_VPN_ACCTS.put(
+    "user_2Ov3lKiWpRKypgbowkua9Icw5rH",
+    JSON.stringify(["bbbefd0b-43b1-41ce-90ff-481dcf25994d"])
+  );
+  console.log(task);
+  return new Response("Hello, Next.js!");
+}

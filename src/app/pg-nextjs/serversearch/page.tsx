2a392110d48d2a46159ceae02b6542ea5af8bdb3
zizfn <1803942+zizifn@users.noreply.github.com>
2023-04-30 17:52:41 +0800

    ignore error

--- src/app/pg-nextjs/serversearch/page.tsx
+++ src/app/pg-nextjs/serversearch/page.tsx
@@ -18,6 +18,7 @@ export default function ServerSearch({ searchParams }: { searchParams: any }) {
       <h2 className="h-10 text-xl text-cyan-400">server search demo</h2>
       <SearchableRepo search={search}></SearchableRepo>
       <Suspense fallback={<div>loading repo...</div>}>
+        {/* @ts-expect-error */}
         <Repos searchParams={searchParams}></Repos>
       </Suspense>
     </>

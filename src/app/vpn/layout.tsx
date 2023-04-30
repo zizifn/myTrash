ff51660ec28964a1c5c1ba2a6d5fdc1bc098970f
zizifn <515868058@qq.com>
2023-05-01 03:05:40 +0800

    add vpn page

--- src/app/vpn/layout.tsx
+++ src/app/vpn/layout.tsx
@@ -1,11 +1,20 @@
-export default function RootLayout({
+import { CFWokersEnv } from "../env";
+import VPNAccounts from "./vpn-account";
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const task1 = await (process.env as any as CFWokersEnv).MY_VPN_ACCTS.get(
+    "user_2Ov3lKiWpRKypgbowkua9Icw5rH"
+  );
+
+  console.log(task1);
   return (
     <>
       <div className="m-0 flex h-full w-full flex-auto flex-row items-stretch justify-center p-0">
+        <VPNAccounts></VPNAccounts>
         {children}
       </div>
     </>

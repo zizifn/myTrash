2a54dda85062a7794aa5472e9b285d0a70eea2cd
zizifn <515868058@qq.com>
2023-05-01 03:59:33 +0800

    add kv

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,5 +1,6 @@
 interface CFWokersEnv {
   MY_VPN_ACCTS: KVNamespace;
+  VPN_SUB: KVNamespace;
 }
 
 export type { CFWokersEnv };
--- src/app/vpn/layout.tsx
+++ src/app/vpn/layout.tsx
@@ -6,11 +6,11 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const task1 = await (process.env as any as CFWokersEnv).MY_VPN_ACCTS.get(
+  console.log(process.env as any as CFWokersEnv);
+  const task = await (process.env as any as CFWokersEnv).MY_VPN_ACCTS.get(
     "user_2Ov3lKiWpRKypgbowkua9Icw5rH"
   );
-
-  console.log(task1);
+  console.log(task);
   return (
     <>
       <div className="m-0 flex h-full w-full flex-auto flex-row items-stretch justify-center p-0">

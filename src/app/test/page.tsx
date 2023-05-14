e59c7d5f58f188f593b8bb4cc07ff21bfe9d6a17
zizfn <1803942+zizifn@users.noreply.github.com>
2023-05-14 18:02:38 +0800

    read kv

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,20 @@
+import { CFWokersEnv } from "../env";
+
 export const runtime = "edge";
 
-export default function Page() {
+export default async function Page() {
+  const users =
+    (await (process.env as any as CFWokersEnv).MY_VPN_ACCTS?.get("users")) ||
+    "[]";
+  const usersJson: string[] = JSON.parse(users);
+
   return (
     <>
+      <ul>
+        {usersJson?.map((user, index) => {
+          return <li key={index}>{user}</li>;
+        })}
+      </ul>
       <div>hello world</div>
     </>
   );

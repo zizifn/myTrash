90c282a2b4c209582814c36755e2c4c1009e75e9
zizfn <1803942+zizifn@users.noreply.github.com>
2023-05-14 19:09:26 +0800

    remove D1 in code

--- src/app/api/mock/d1/route.ts
+++ src/app/api/mock/d1/route.ts
@@ -1,16 +1,21 @@
 import { CFWokersEnv } from "@/app/env";
+import { NextResponse } from "next/server";
 export const runtime = "edge";
 export async function GET(request: Request) {
-  console.log(process.env as any as CFWokersEnv);
-  const authDB = await (process.env as any as CFWokersEnv).NEXT_AUTH_DB;
-  const result = authDB.batch([
-    authDB
-      .prepare("INSERT INTO user (id, name, age) VALUES (?1, ?2, ?3)")
-      .bind("0c916c0b-4014-4fd2-aa0b-007071b3c743", "james", 19),
-    authDB
-      .prepare("INSERT INTO user (id, name, age) VALUES (?1, ?2, ?3)")
-      .bind("fbe986e6-19b3-4fc5-8758-30d8864704e0", "zizifn", 20),
-  ]);
-  console.log(result);
-  return new Response("Hello, Next.js!");
+  // console.log(process.env as any as CFWokersEnv);
+  // const authDB = (process.env as any as CFWokersEnv).NEXT_AUTH_DB;
+
+  // const ps = authDB.prepare("SELECT * from user");
+  // const data = await ps.first();
+
+  // const result = await authDB.batch([
+  //   authDB
+  //     .prepare("INSERT INTO user (id, name, age) VALUES (?1, ?2, ?3)")
+  //     .bind("0c916c0b-4014-4fd2-aa0b-007071b3c743", "james", 19),
+  //   authDB
+  //     .prepare("INSERT INTO user (id, name, age) VALUES (?1, ?2, ?3)")
+  //     .bind("fbe986e6-19b3-4fc5-8758-30d8864704e0", "zizifn", 20),
+  // ]);
+  // console.log(data);
+  return NextResponse.json({});
 }
